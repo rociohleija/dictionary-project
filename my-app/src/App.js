93644fdc94fcf15d6f7b48fb797422e4a018395e
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-10-05 12:20:04 -0500

    Build the search engine to a new component Dictionary

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,34 @@
 import Letters from './Letters.jpg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Dictionary from './Dictionary'
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={Letters} 
         className="App-Letters" 
         alt="image" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer text-center">
+        This project was coded by {" "}
+      <a href="https://astonishing-pixie-d099a0.netlify.app/" target="_blank" rel="noopener noreferre">
+        Rocio Hernandez
+      </a>{" "}
+      and is{" "}
+      <a href="https://github.com/rociohleija/dictionary-project"
+      target="_blank"
+      re="noopener noreferre"
+      >
+        open-source on GitHub.
+      </a>
+      </footer>
+      </div>
     </div>
   );
 }
