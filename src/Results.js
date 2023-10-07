1fe92ff6902ca4c4e316eca3b8b7ed23e0300123
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-10-06 21:01:46 -0500

    Change the CSS Results component

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
-import './Results.ccs';
+import './Results.css';
 
 export default function Results(props) {
     if (props.results) {
